331bb4f5a8f32f212b9e3cfcc27bff60d3ca95f7
melikakhalili <melikakhalili2500@gmail.com>
2024-12-19 17:23:35 +0330

    feat:creatTask

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
-const taskName =document.getElementById("taskName")
-const priority = document.getElementById("priority")
-const tableData = document.getElementById("tableData")
-const baseurl="https://67618d7346efb37323722400.mockapi.io/api/v1/"
-
+const taskName = document.getElementById("taskName").value;
+const priority = document.getElementById("priority").value;
+const tableData = document.getElementById("tableData");
+const form = document.getElementById("form");
+const status = document.getElementById("status").value;
+const baseurl = "https://67618d7346efb37323722400.mockapi.io/api/v1/";
 async function getTask() {
-    try {
-        const response=await fetch(`${baseurl}tasks`)
-        console.log(response)
-        console.log(response.JSON)
-        const data=await response.json()
-        console.log(data)
-        renderTask(data)
-        return data
-
-    } catch (error) {
-        console.log(error)
-    }
-    
+  try {
+    const response = await fetch(`${baseurl}tasks`);
+    const data = await response.json();
+    renderTask(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getTask();
 
 function renderTask(data) {
-    tableData.innerHTML=""
-    tableData.innerHTML+=data.map(toDo => {
-        return`
+  tableData.innerHTML = "";
+  tableData.innerHTML += data.map((toDo) => {
+    return `
         <tr>
       <td>
  ${toDo.taskName}
       </td>
       <td>
-
+${toDo.priority}
       </td>
       <td>
-
+${toDo.status}
       </td>
       <td>
+<button>👁️</button>
+<button>❌</button>
+
 
       </td>
-       </tr>`
+       </tr>`;
+  });
+}
+form.addEventListener("submit", creatTodo());
+
+async function creatTodo() {
+  try {
+    const response = await fetch(`${baseurl}tasks`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        taskName,
+        status,
+        priority,
+      }),
     });
+  } catch (error) {
+    console.log(error);
+  }
 }
